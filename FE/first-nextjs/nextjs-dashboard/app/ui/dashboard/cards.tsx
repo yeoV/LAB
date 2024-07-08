a471c726ab6da9b3ee9b85d615fddf051b19a088
yeoV <ys_lee1017@naver.com>
2024-07-08 15:31:51 +0900

    CH7. Fetching data - varcel - promise 를 통한 비동기 data fetch - React Server Component 룰 통한 data fetch

--- FE/first-nextjs/nextjs-dashboard/app/ui/dashboard/cards.tsx
+++ FE/first-nextjs/nextjs-dashboard/app/ui/dashboard/cards.tsx
@@ -13,19 +13,29 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
+export default async function CardWrapper({
+  totalPaidInvoices,
+  totalPendingInvoices,
+  numberOfInvoices,
+  numberOfCustomers,
+}:{
+  totalPaidInvoices: string;
+  totalPendingInvoices: string;
+  numberOfInvoices: number;
+  numberOfCustomers: number;
+}) {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
